777a89d9d7cd3978b982ed0e291c5db8dbf5e2ff
Toni Jovanoski <minimoog77@gmail.com>
2017-02-28 00:30:58 +0100

    Add mid point displacement function

--- Landscape2dgen/ViewController.swift
+++ Landscape2dgen/ViewController.swift
@@ -8,6 +8,53 @@
 
 import UIKit
 
+struct Point {
+    let x: Float
+    let y: Float
+}
+
+extension Array {
+    func sample() -> Element {
+        let index = Int(arc4random_uniform(UInt32(self.count)))
+        
+        return self[index]
+    }
+}
+
+func midPointDisplacement(start: Point, end: Point, roughtness: Float, verticalDisplacement: Float = 0.0, numOfIterations: Int = 16) -> [Point] {
+    var vd = verticalDisplacement
+    
+    if verticalDisplacement == 0.0 {
+        vd = (start.y + end.y) / 2.0
+    }
+    
+    var points = [start, end]
+    
+    var iteration = 1
+    
+    while iteration <= numOfIterations {
+        let p = points
+        
+        for i in 0..<p.count - 1 {
+            let midpoint = Point(x: (p[i].x + p[i+1].x) / 2.0,
+                                 y: (p[i].y + p[i+1].y) / 2.0)
+            
+            
+            let dispMidpoint = Point(x: midpoint.x,
+                                     y: midpoint.y + [-vd, vd].sample())
+            
+            points.insert(dispMidpoint, at: 2 * i + 1)
+            
+        }
+        
+        vd *= pow(2, -roughtness)
+        
+        iteration += 1
+    }    
+    return points
+}
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
